eaf165bcd5c2167190113ad48be7cf90deec0647
funnysuslik <stepanov.step@gmail.com>
2025-08-18 23:22:20 +0400

    fix/Better(?) naming

--- backend/app/crud/users.py
+++ backend/app/crud/users.py
@@ -5,25 +5,25 @@ from app.models.users import User, UserCreate
 
 
 def create_user(*, session: Session, user_create: UserCreate) -> User:
-    db_obj = User.model_validate(
+    user = User.model_validate(
         user_create, update={"hashed_password": get_password_hash(user_create.password)}
     )
-    session.add(db_obj)
+    session.add(user)
     session.commit()
-    session.refresh(db_obj)
-    return db_obj
+    session.refresh(user)
+    return user
 
 
 def get_user_by_email(*, session: Session, email: str) -> User | None:
-    statement = select(User).where(User.email == email)
-    session_user = session.exec(statement).first()
-    return session_user
+    q = select(User).where(User.email == email)
+    user = session.exec(q).first()
+    return user
 
 
 def authenticate(*, session: Session, email: str, password: str) -> User | None:
-    db_user = get_user_by_email(session=session, email=email)
-    if not db_user:
+    user = get_user_by_email(session=session, email=email)
+    if not user:
         return None
-    if not verify_password(password, db_user.hashed_password):
+    if not verify_password(password, user.hashed_password):
         return None
-    return db_user
+    return user
